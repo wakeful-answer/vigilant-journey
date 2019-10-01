b6388fa89f1e9aaa59359847a7e6d291e540b901
garden <patsylasca@gmail.com>
2019-10-02 04:08:54 +0800

    og image

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,14 @@ module.exports = {
   head: [
     [ "link",{ rel: "stylesheet",href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"}],
     [ "link",{ rel: "stylesheet",href: "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/tiny-slider.css"}],
-    [ "meta",{ name: "viewport", content: "width=device-width,initial-scale=1.0"}]
+    // [ "link",{ rel: "stylesheet",href: "/jquery.skedTape.css"}],
+    [ "meta",{ name: "viewport", content: "width=device-width,initial-scale=1.0"}],
+    [ "meta",{ property: "og:url", content: "http://www.wsk.io"}],
+    [ "meta",{ property: "og:type", content: "website"}],
+    [ "meta",{ property: "og:description", content: "WSK X"}],
+    [ "meta",{ property: "og:title", content: "Festival of The Recently Possible"}],
+    [ "meta",{ property: "og:image", content: "/og-image.jpg"}]
+    // ['script', {src: 'https://code.jquery.com/jquery-3.4.1.min.js'}]
   ],
   themeConfig: {
     nav: [
